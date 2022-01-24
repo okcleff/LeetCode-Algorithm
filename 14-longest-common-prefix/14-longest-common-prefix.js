a1bba176c693be908c8080b9f71b95e5b77e8097
okcleff <85351198+okcleff@users.noreply.github.com>
2022-01-24 16:24:53 +0900

    Time: 118 ms (25.28%), Space: 39.9 MB (71.15%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if (strs.length === 1) {
+        return strs[0];
+    } else {
+        for (let i = 1; i < strs.length; i++) {
+            for (let j = 0; j < strs[0].length; j++) {
+                if (strs[0][j] !== strs[i][j]) {
+                    strs[0] = strs[0].slice(0, j);
+                }
+            }
+        }
+    }
+  return strs[0];
+};
