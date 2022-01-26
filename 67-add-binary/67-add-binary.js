7a6aca50dd0d91628952ed66ce60f7237b688b0a
okcleff <85351198+okcleff@users.noreply.github.com>
2022-01-26 12:04:08 +0900

    Time: 92 ms (58.83%), Space: 39.8 MB (94.81%) - LeetHub

--- 67-add-binary/67-add-binary.js
+++ 67-add-binary/67-add-binary.js
@@ -4,18 +4,6 @@
  * @return {string}
  */
 
-// const bin = (a) => {
-//     let result = 0;
-//     for (let i = 0; i < a.length; i++) {
-//         result = result + parseInt(a[i]) * Math.pow(2, a.length - 1 - i);
-//     }
-//     return result;
-// };
-
-// const big = (a) => {
-//     return BigInt(a);
-// }
-
 var addBinary = function(a, b) {
     return (BigInt('0b'+a) + BigInt('0b'+b)).toString(2);
 };
