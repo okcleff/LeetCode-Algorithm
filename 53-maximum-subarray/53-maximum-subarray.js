291f3e66476cba51bb1b3b0a38289db9b26d2c5c
okcleff <85351198+okcleff@users.noreply.github.com>
2022-02-06 18:06:47 +0900

    Time: 80 ms (97.92%), Space: 50.6 MB (14.56%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -3,39 +3,9 @@
  * @return {number}
  */
 
-// function reducer(array) {
-//   if (array.length === 1) {
-//     return array[0];
-//   } else {
-//     let result = Number.MIN_SAFE_INTEGER;
-//     array.reduce(function (a, b) {
-//       return (result = Math.max(a, b));
-//     });
-//     return result;
-//   }
-// }
 
-var maxSubArray = function(nums) {
-//   if (nums.length === 1) {
-//     return nums[0];
-//   } else {
-//     let arr = [...nums];
-//     for (let i = 0; i < nums.length; i++) {
-//       let sum = nums[i];
-//       for (let j = i + 1; j < nums.length; j++) {
-//         sum = sum + nums[j];
-//         arr.push(sum);
-//       }
-//     }
-    
-//     if (reducer(arr) > 0) {
-//       return reducer(arr);
-//     } else {
-//       return reducer(nums);
-//     }
-//   }
-    
-    if(nums.length == 0) return 0;
+var maxSubArray = function(nums) {    
+    if(nums.length === 0) return 0;
     let result = Number.MIN_SAFE_INTEGER;
     let sum = 0;
     for(let i = 0; i < nums.length; i++) {
