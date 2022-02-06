09eb78c4c638a988c756732ca09ac636aab7ce06
okcleff <85351198+okcleff@users.noreply.github.com>
2022-02-06 18:32:41 +0900

    Time: 64 ms (96.49%), Space: 42.2 MB (9.32%) - LeetHub

---
+++ 27-remove-element/27-remove-element.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+  for (let i = nums.length; i >= 0; i--) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+    }
+  }
+};
