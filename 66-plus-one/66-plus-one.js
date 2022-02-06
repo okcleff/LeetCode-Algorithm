e078675ba0ed195cbb7673d5d9921aea51d5670c
okcleff <85351198+okcleff@users.noreply.github.com>
2022-02-06 19:17:16 +0900

    Time: 78 ms (60.27%), Space: 42 MB (10.32%) - LeetHub

--- 66-plus-one/66-plus-one.js
+++ 66-plus-one/66-plus-one.js
@@ -7,3 +7,15 @@ var plusOne = function(digits) {
   const result = arr.map((el) => parseInt(el));
   return result;
 };
+
+// var plusOne = function(digits) {
+//   for(let i = digits.length - 1; i >= 0; i--) {
+//     if(digits[i] === 9) {
+//       digits[i] = 0;
+//     } else {
+//       digits[i] ++;
+//       return digits;
+//     }
+//   }
+//   return [1, ...digits];
+// };
