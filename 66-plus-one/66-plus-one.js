140369cd7c7e54d9b5ec818e5d09331d4d7c6802
okcleff <85351198+okcleff@users.noreply.github.com>
2022-02-07 12:50:16 +0900

    Time: 88 ms (49.87%), Space: 42.1 MB (9.98%) - LeetHub

--- 66-plus-one/66-plus-one.js
+++ 66-plus-one/66-plus-one.js
@@ -2,20 +2,20 @@
  * @param {number[]} digits
  * @return {number[]}
  */
-var plusOne = function(digits) {
-  const arr = (BigInt([...digits].join('')) + BigInt(1)).toString().split('');
-  const result = arr.map((el) => parseInt(el));
-  return result;
-};
-
 // var plusOne = function(digits) {
-//   for(let i = digits.length - 1; i >= 0; i--) {
-//     if(digits[i] === 9) {
-//       digits[i] = 0;
-//     } else {
-//       digits[i] ++;
-//       return digits;
-//     }
-//   }
-//   return [1, ...digits];
+//   const arr = (BigInt([...digits].join('')) + BigInt(1)).toString().split('');
+//   const result = arr.map((el) => parseInt(el));
+//   return result;
 // };
+
+var plusOne = function(digits) {
+  for(let i = digits.length - 1; i >= 0; i--) {
+    if(digits[i] === 9) {
+      digits[i] = 0;
+    } else {
+      digits[i] ++;
+      return digits;
+    }
+  }
+  return [1, ...digits];
+};
