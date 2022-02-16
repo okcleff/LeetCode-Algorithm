545a1f8403f6402ee83b0797af99335fd8f8e5fe
okcleff <85351198+okcleff@users.noreply.github.com>
2022-02-16 14:03:01 +0900

    Time: 111 ms (54.69%), Space: 44.1 MB (10.25%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.js
+++ 28-implement-strstr/28-implement-strstr.js
@@ -3,26 +3,14 @@
  * @param {string} needle
  * @return {number}
  */
-var strStr = function(haystack, needle) {
-  if(needle.length === 0) return 0;
-
-    
-
-	for(let i = 0; i < haystack.length; i++) {
-
-    if(haystack[i] === needle[0]) {
-
-      if(haystack.slice(i, i+needle.length) === needle) {
-
-        return i
-
+var strStr = function (haystack, needle) {
+  if (needle.length === 0) return 0;
+  for (let i = 0; i < haystack.length; i++) {
+    if (haystack[i] === needle[0]) {
+      if (haystack.slice(i, i + needle.length) === needle) {
+        return i;
       }
-
     }
-
   }
-
-    
-
-  return -1
+  return -1;
 };
