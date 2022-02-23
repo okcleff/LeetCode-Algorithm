3ed3637dbb4937458bf1c13ad276e5ebe762b227
okcleff <85351198+okcleff@users.noreply.github.com>
2022-02-23 13:33:40 +0900

    Time: 90 ms (74.47%), Space: 45.9 MB (32.89%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+  const regex = /[^A-Za-z0-9]/g;
+  const str = s.replace(regex, '').toLowerCase();
+  const reverse = str.split('').reverse().join('').toString();
+  return str === reverse;
+};
