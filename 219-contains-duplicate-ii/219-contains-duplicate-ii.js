415bf0ee876c7fba1351491c028b5f3e4b693646
okcleff <85351198+okcleff@users.noreply.github.com>
2022-02-24 16:28:12 +0900

    Time: 96 ms (98.62%), Space: 63.5 MB (36.95%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.js
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.js
@@ -4,7 +4,10 @@
  * @return {boolean}
  */
 var containsNearbyDuplicate = function(nums, k) {
-  var obj = {};
+  const set = new Set(nums);
+  if (nums.length === [...set].length) return false;
+
+  let obj = {};
   for (let i = 0; i < nums.length; i++) {
     if (obj[nums[i]] >= 0 && i - obj[nums[i]] <= k) return true;
     obj[nums[i]] = i;
