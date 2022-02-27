1c7e5ef96a0c4fdd7cfd6272eb1711cb0f0f9515
okcleff <85351198+okcleff@users.noreply.github.com>
2022-02-27 16:49:06 +0900

    Time: 110 ms (50.00%), Space: 44.2 MB (6.33%) - LeetHub

---
+++ 263-ugly-number/263-ugly-number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isUgly = function(n) {
+  if (n <= 0) return false;
+  if (n === 1) return true;
+  if (n % 2 === 0) return isUgly(n / 2);
+  else if (n % 3 === 0) return isUgly(n / 3);
+  else if (n % 5 === 0) return isUgly(n / 5);
+  else return false;
+};
