4d78caf948dd0c4bbdd49b80137b21ea99faa743
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2022-03-04 21:31:38 +0900

    Time: 86 ms (67.78%), Space: 43.6 MB (61.32%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.js
+++ 389-find-the-difference/389-find-the-difference.js
@@ -4,14 +4,19 @@
  * @return {character}
  */
 var findTheDifference = function(s, t) {
-  const hash = (str) => {
-    let obj = {};
-    for (let i = 0; i < str.length; i++) {
-      obj[str[i]] ? (obj[str[i]] = obj[str[i]] + 1) : (obj[str[i]] = 1);
-    }
-    return obj;
-  };
-  for (let key in hash(t)) {
-    if (hash(s)[key] !== hash(t)[key]) return key;
+  // const hash = (str) => {
+  //   let obj = {};
+  //   for (let i = 0; i < str.length; i++) {
+  //     obj[str[i]] ? (obj[str[i]] = obj[str[i]] + 1) : (obj[str[i]] = 1);
+  //   }
+  //   return obj;
+  // };
+  // for (let key in hash(t)) {
+  //   if (hash(s)[key] !== hash(t)[key]) return key;
+  // }
+  const arr1 = s.split('').sort();
+  const arr2 = t.split('').sort();
+  for (let i = 0; i < arr2.length; i++) {
+    if (arr2[i] !== arr1[i]) return arr2[i];
   }
 };
