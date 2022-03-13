7ef0c5a8764ec6076b236aa1b370d6a973d38968
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2022-03-13 11:34:38 +0900

    Time: 81 ms (65.40%), Space: 42 MB (63.34%) - LeetHub

---
+++ 771-jewels-and-stones/771-jewels-and-stones.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+var numJewelsInStones = function(jewels, stones) {
+  const obj = {};
+  const arr = stones.split('');
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (jewels.indexOf(arr[i]) === -1) {
+      arr.splice(i, 1);
+    }
+  }
+  return arr.length;
+};
