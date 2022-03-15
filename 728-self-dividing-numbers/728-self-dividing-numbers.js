af9593f1010386547b6063f2c431b73509a7974f
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2022-03-15 15:55:15 +0900

    Time: 152 ms (8.62%), Space: 45.3 MB (25.86%) - LeetHub

---
+++ 728-self-dividing-numbers/728-self-dividing-numbers.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number[]}
+ */
+var selfDividingNumbers = function(left, right) {
+  const isSelfDividing = (num) => {
+    const str = num.toString();
+    for (let i = 0; i < str.length; i++) {
+      if (num % parseInt(str[i]) !== 0) return;
+    }
+    return num;
+  };
+
+  const arr = [];
+  for (let i = left; i < right + 1; i++) {
+    arr.push(i);
+  }
+
+  const result = arr.filter((el) => {
+    return isSelfDividing(el);
+  });
+
+  return result;
+};
