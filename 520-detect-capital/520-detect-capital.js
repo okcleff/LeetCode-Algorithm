81668e24a1bda96fed80f1e2e7447a44eee09d9c
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2022-03-25 01:30:17 +0900

    Time: 83 ms (82.31%), Space: 42.3 MB (10.33%) - LeetHub

--- 520-detect-capital/520-detect-capital.js
+++ 520-detect-capital/520-detect-capital.js
@@ -3,5 +3,13 @@
  * @return {boolean}
  */
 var detectCapitalUse = function(word) {
-    return /^[A-Z]+$/.test(word) || /^[A-Z][a-z]+$/.test(word) || /^[a-z]+$/.test(word);
+    if (word === word.toUpperCase()) return true;
+    if (word === word.toLowerCase()) return true;
+    
+    if (word[0] === word[0].toUpperCase()) {
+        let leftOutStr = word.slice(1);
+        if (leftOutStr === leftOutStr.toLowerCase()) return true;
+    }
+    
+    return false;
 };
