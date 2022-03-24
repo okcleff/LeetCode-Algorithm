4ddcb82ffbb4832e51555f77387dcc81e481444f
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2022-03-24 14:42:16 +0900

    Time: 100 ms (98.82%), Space: 47.3 MB (72.94%) - LeetHub

---
+++ 561-array-partition-i/561-array-partition-i.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var arrayPairSum = function (nums) {
+  nums.sort((a, b) => a - b);
+  let result = 0;
+  for (let i = 0; i < nums.length; i += 2) {
+    result += nums[i];
+  }
+  return result;
+};
