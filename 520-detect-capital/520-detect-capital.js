1d82def54b323094aa899f3b98c2ca2fab6a7d30
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2022-03-25 01:25:29 +0900

    Time: 88 ms (76.29%), Space: 42.1 MB (13.72%) - LeetHub

---
+++ 520-detect-capital/520-detect-capital.js
@@ -0,0 +1,7 @@
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var detectCapitalUse = function(word) {
+    return /^[A-Z]+$/.test(word) || /^[A-Z][a-z]+$/.test(word) || /^[a-z]+$/.test(word);
+};
