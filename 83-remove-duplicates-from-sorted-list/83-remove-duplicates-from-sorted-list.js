42ef32cbda2ac53b4e72644cbf78d250abe974ba
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2022-04-07 00:41:52 +0900

    Time: 77 ms (77.99%), Space: 44.4 MB (52.23%) - LeetHub

---
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    if(!head) return null;
+    
+    let cur = head;
+    
+    while(cur.next) {
+        if(cur.val === cur.next.val) {
+            cur.next = cur.next.next    
+        } else {
+            cur = cur.next
+        }
+        
+    }
+    
+    return head;
+};
