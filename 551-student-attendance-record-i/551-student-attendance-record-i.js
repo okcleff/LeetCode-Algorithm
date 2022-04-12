628c2f2a0c3fb65d56b2315cc52f5914ae34f8d6
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2022-04-12 21:56:01 +0900

    Time: 64 ms (87.03%), Space: 42.1 MB (63.24%) - LeetHub

--- 551-student-attendance-record-i/551-student-attendance-record-i.js
+++ 551-student-attendance-record-i/551-student-attendance-record-i.js
@@ -3,11 +3,12 @@
  * @return {boolean}
  */
 var checkRecord = function(s) {
+    if(s.indexOf('LLL') !== -1) return false;
+    
     let absence = 0;
     for(let i = 0; i < s.length; i++){
         if(s[i] === 'A') absence++;
     }
-    if(absence >= 2) return false;
-    if(s.indexOf('LLL') !== -1) return false;
-    return true;
+    
+    return absence >= 2 ? false: true;
 };
