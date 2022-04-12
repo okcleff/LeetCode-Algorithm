7ff895b7fbaddc244f7ce70f2859f6aa2e2250fe
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2022-04-12 13:08:09 +0900

    Time: 89 ms (46.27%), Space: 42.4 MB (68.00%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.js
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.js
@@ -4,6 +4,6 @@
  */
 var finalValueAfterOperations = function(operations) {
     let answer = 0;
-    operations.map(el => el.indexOf('+') !== -1 ? answer++ : answer--)
+    operations.map(el => el.indexOf('-') === -1 ? answer++ : answer--)
     return answer;
 };
