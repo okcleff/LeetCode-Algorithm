d4f6d6f2271d451d2185b64368453d6099e27f89
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2022-04-15 02:15:06 +0900

    Time: 56 ms (95.10%), Space: 42.1 MB (23.67%) - LeetHub

--- 434-number-of-segments-in-a-string/434-number-of-segments-in-a-string.js
+++ 434-number-of-segments-in-a-string/434-number-of-segments-in-a-string.js
@@ -3,11 +3,6 @@
  * @return {number}
  */
 var countSegments = function(s) {
-  // if (!s.match(/[a-zA-Z]/g)) return 0;
-  // return s
-  //   .trim()
-  //   .replace(/\s{1,}/g, ' ')
-  //   .split(' ').length;
     return s.trim()
     ? s
         .trim()
