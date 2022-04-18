6845fe7f98bd4f0e4c28021d0319a1fe49cd88ac
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2022-04-19 00:31:38 +0900

    Time: 383 ms (76.28%), Space: 56.7 MB (80.00%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.js
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.js
@@ -6,11 +6,10 @@ var minPairSum = function(nums) {
   if (nums.length === 2) return nums[0] + nums[1];
 
   nums.sort((a, b) => a - b);
-  
-  const pairArr = [];
+  let result = 0;
   for (let i = 0; i < nums.length / 2; i++) {
-    pairArr.push(nums[i] + nums[nums.length - 1 - i]);
+    result = Math.max(result, nums[i] + nums[nums.length - 1 - i]);
   }
-  
-  return Math.max(...pairArr);
+
+  return result;
 };
