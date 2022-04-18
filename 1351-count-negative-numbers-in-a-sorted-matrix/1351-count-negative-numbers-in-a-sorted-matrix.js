72023ea582acf6722c1ba5974c18ecfcc8edaeaa
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2022-04-18 14:19:30 +0900

    Time: 86 ms (46.99%), Space: 46.2 MB (9.59%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.js
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.js
@@ -3,8 +3,16 @@
  * @return {number}
  */
 var countNegatives = function(grid) {
-  let count = 0;
-  const concat = grid.reduce((a, b) => [...a, ...b]);
-  concat.map((num) => (num < 0 ? count++ : 0));
-  return count;
+  // let count = 0;
+  // grid.map((arr) => {
+  //   arr.map((num) => (num < 0 ? count++ : null));
+  // });
+  // return count;
+  
+  // let count = 0;
+  // const concat = grid.reduce((a, b) => [...a, ...b]);
+  // concat.map((num) => (num < 0 ? count++ : 0));
+  // return count;
+  
+  return grid.flat().filter(num => num < 0).length;
 };
