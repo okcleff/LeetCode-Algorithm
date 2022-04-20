293aa4ac34b3d4b9f4b199b56451864b64375d58
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2022-04-20 19:45:22 +0900

    Time: 142 ms (86.41%), Space: 58.6 MB (93.20%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.js
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.js
@@ -3,16 +3,18 @@
  * @return {number}
  */
 var minSetSize = function(arr) {
-  let dict = {};
+  const dict = {};
   arr.map((num) => (dict[num] = (dict[num] || 0) + 1));
 
-  let size = arr.length;
+  const half = arr.length / 2
   let set = 0;
   const freq = Object.values(dict).sort((a, b) => a - b);
   let sum = freq.reduce((a, b) => a + b);
-  while (sum > arr.length / 2) {
+  
+  while (sum > half) {
     sum -= freq.pop();
     set++;
   }
+  
   return set;
 };
