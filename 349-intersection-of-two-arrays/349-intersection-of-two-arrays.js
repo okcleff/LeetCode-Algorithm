3426ae4639c12876142bc45bb5e9d4ffe96c5083
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2022-05-09 08:47:27 +0900

    Time: 127 ms (5.25%), Space: 43.7 MB (47.96%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.js
@@ -7,8 +7,11 @@ var intersection = function(nums1, nums2) {
   const set1 = new Set(nums1);
   const set2 = new Set(nums2);
   let answer = [];
-  [...set1].map((num) => {
+  // [...set1].map((num) => {
+  //   if (set2.has(num)) answer.push(num);
+  // });
+  for(let num of set1){
     if (set2.has(num)) answer.push(num);
-  });
+  }
   return answer;
 };
