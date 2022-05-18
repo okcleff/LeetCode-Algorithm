3d76191f29883d631f0354facef5e14cdf6c2f18
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2022-05-18 15:46:59 +0900

    Time: 113 ms (7.43%), Space: 42.9 MB (28.27%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.js
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.js
@@ -3,7 +3,5 @@
  * @return {number}
  */
 var peakIndexInMountainArray = function(arr) {
-  for(let i = 1; i < arr.length - 1; i++){
-    if(arr[i] > arr[i + 1]) return i;
-  }
+  return arr.indexOf(Math.max(...arr));
 };
