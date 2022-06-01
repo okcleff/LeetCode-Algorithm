bd5645af2f8c7c009330d4994aa10b46c75604dc
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2022-06-01 12:00:58 +0900

    Time: 89 ms (33.46%), Space: 42.2 MB (26.50%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.js
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.js
@@ -7,9 +7,7 @@ var numIdenticalPairs = function(nums) {
 
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] === nums[j]) {
-        count++;
-      }
+      if (nums[i] === nums[j]) count++;
     }
   }
   return count;
