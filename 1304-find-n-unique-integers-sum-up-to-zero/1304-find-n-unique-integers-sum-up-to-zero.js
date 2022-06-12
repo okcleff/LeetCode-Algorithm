eee785c202b9073107a0fd1b7d17d604098cfdbd
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2022-06-12 23:17:54 +0900

    Time: 114 ms (11.72%), Space: 42.9 MB (12.50%) - LeetHub

--- 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.js
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.js
@@ -6,12 +6,12 @@ var sumZero = function(n) {
   let answer = [];
   if (n % 2 === 0) {
     for (let i = 1; i <= n * 0.5; i++) {
-      answer = [...answer, i, -i];
+      answer.push(i, -i);
     }
   } else {
     answer.push(0);
     for (let i = 1; i < n * 0.5; i++) {
-      answer = [...answer, i, -i];
+      answer.push(i, -i);
     }
   }
   return answer;
