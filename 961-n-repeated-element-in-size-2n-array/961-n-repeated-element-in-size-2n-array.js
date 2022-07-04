81f422a62c542af791d40708ba58408f8738fa43
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2022-07-04 09:50:17 +0900

    Time: 135 ms (28.92%), Space: 46.1 MB (54.46%) - LeetHub

--- 961-n-repeated-element-in-size-2n-array/961-n-repeated-element-in-size-2n-array.js
+++ 961-n-repeated-element-in-size-2n-array/961-n-repeated-element-in-size-2n-array.js
@@ -5,10 +5,6 @@
 var repeatedNTimes = function(nums) {
 	nums.sort((a, b) => a - b);
 	for (let i = 0; i < nums.length; i++) {
-		if (
-			nums.lastIndexOf(nums[i]) - nums.indexOf(nums[i]) + 1 ===
-			nums.length / 2
-		)
-			return nums[i];
+		if (nums[i] === nums[i+1]) return nums[i];
 	}
 };
