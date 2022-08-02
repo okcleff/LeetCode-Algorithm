c16e76109dd132ac6db0e02858aafa3077d9c4d4
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2022-08-02 09:54:09 +0900

    Time: 159 ms (6.80%), Space: 48.5 MB (9.60%) - LeetHub

---
+++ 807-max-increase-to-keep-city-skyline/807-max-increase-to-keep-city-skyline.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxIncreaseKeepingSkyline = function(grid) {
+	let leng = grid.length;
+	let answer = 0;
+	for (let i = 0; i < leng; i++) {
+		for (let j = 0; j < leng; j++) {
+			let row = grid[i];
+			let column = grid.map((row) => {
+				return row[j];
+			});
+			answer += Math.min(Math.max(...row), Math.max(...column)) - grid[i][j];
+		}
+	}
+	return answer;
+};
