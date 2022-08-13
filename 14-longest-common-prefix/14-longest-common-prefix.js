75ee7a26e3b2da1a3aa9002f12027ede56d853dd
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2022-08-13 23:34:26 +0900

    Time: 66 ms (94.55%), Space: 42.6 MB (49.71%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.js
+++ 14-longest-common-prefix/14-longest-common-prefix.js
@@ -3,16 +3,14 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    if (strs.length === 1) {
-        return strs[0];
-    } else {
-        for (let i = 1; i < strs.length; i++) {
-            for (let j = 0; j < strs[0].length; j++) {
-                if (strs[0][j] !== strs[i][j]) {
-                    strs[0] = strs[0].slice(0, j);
-                }
-            }
-        }
-        return strs[0];
+  if(strs.length === 1) return strs[0]; 
+  let answer = '';
+  for (let i = 1; i <= strs[0].length; i++) {
+    let prefix = strs[0].slice(0, i);
+    for (let j = 0; j < strs.length; j++) {
+      if (!strs[j].startsWith(prefix)) prefix = answer;
     }
+    answer = prefix;
+  }
+  return answer;
 };
