22ec9d0a12e72f7f1676e59b357ccc9595b1224d
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2022-08-16 10:30:30 +0900

    Time: 99 ms (33.65%), Space: 42.4 MB (91.59%) - LeetHub

---
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string[]} words
+ * @param {string} pref
+ * @return {number}
+ */
+var prefixCount = function(words, pref) {
+  return words.filter((word) => word.startsWith(pref)).length;
+};
