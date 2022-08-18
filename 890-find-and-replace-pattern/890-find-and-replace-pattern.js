03dc48192cb80e283c435808100bf562147ed197
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2022-08-18 09:12:56 +0900

    Time: 83 ms (73.43%), Space: 44 MB (73.43%) - LeetHub

---
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string[]} words
+ * @param {string} pattern
+ * @return {string[]}
+ */
+var findAndReplacePattern = function(words, pattern) {
+	const patternSet = new Set([...pattern]);
+  
+	return words
+		.filter((word) => {
+			const wordSet = new Set([...word]);
+			return wordSet.size === patternSet.size;
+		})
+		.filter((word) => {
+			const wordSet = new Set([...word]);
+
+			let obj = {};
+			[...wordSet].map((char, idx) => {
+				obj[char] = [...patternSet][idx];
+			});
+
+			return (
+				[...word]
+					.map((char) => {
+						return (char = obj[char]);
+					})
+					.join('') === pattern
+			);
+		});
+};
