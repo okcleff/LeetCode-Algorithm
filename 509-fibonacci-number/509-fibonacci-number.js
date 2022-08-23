b5c4cc5eec5fe6a24757bc0dd78ed367af911de8
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2022-08-23 09:48:00 +0900

    Time: 98 ms (51.64%), Space: 41.7 MB (70.97%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.js
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -3,6 +3,11 @@
  * @return {number}
  */
 var fib = function(n) {
-  if (n < 2) return n;
-	return fib(n - 1) + fib(n - 2);
+	const result = [0, 1];
+	for (let i = 2; i <= n; i++) {
+		const a = result[i - 1];
+		const b = result[i - 2];
+		result.push(a + b);
+	}
+	return result[n];
 };
