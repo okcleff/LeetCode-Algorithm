fc8b37dd8cd9d4eb0adcc12b107d581ea177d95e
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2022-08-31 10:57:28 +0900

    Time: 237 ms (75.92%), Space: 68 MB (40.74%) - LeetHub

---
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string[]} garbage
+ * @param {number[]} travel
+ * @return {number}
+ */
+var garbageCollection = function(garbage, travel) {
+	const type = ['G', 'P', 'M'];
+
+	let total = garbage.join('').length;
+
+	const lastIdx = (g) => {
+		let idx = 0;
+		for (let i = garbage.length - 1; i >= 0; i--) {
+			if (garbage[i].includes(g)) {
+				idx = i;
+				return idx;
+			}
+		}
+	};
+
+	type.map((g) => {
+		total += [...travel].splice(0, lastIdx(g)).reduce((c, a) => c + a, 0);
+	});
+
+	return total;
+};
