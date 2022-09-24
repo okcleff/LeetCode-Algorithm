7aa68be2ab2955ec70a2a4670a3f7976addd207a
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2022-09-24 13:34:53 +0900

    Time: 101 ms (25.96%), Space: 42.1 MB (28.37%) - LeetHub

--- 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.js
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.js
@@ -7,8 +7,8 @@ var isPrefixOfWord = function(sentence, searchWord) {
   const arr = sentence.split(' ');
 
   for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
     if (arr[i].startsWith(searchWord)) return i + 1;
   }
+  
   return -1;
 };
