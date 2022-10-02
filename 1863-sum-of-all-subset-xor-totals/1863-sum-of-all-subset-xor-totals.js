97fb4a495f762675767a2567f58b0973b08f7732
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2022-10-02 10:38:10 +0900

    Time: 101 ms (52.25%%), Space: 48.4 MB (25.23%%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.js
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.js
@@ -3,19 +3,14 @@
  * @return {number}
  */
 var subsetXORSum = function(nums) {
-	const subsets = nums.reduce(
-		(subset, value) => subset.concat(subset.map((set) => [value, ...set])),
-		[[]]
-	);
+  const subsets = nums.reduce(
+    (subset, value) => subset.concat(subset.map((set) => [value, ...set])),
+    [[]]
+  );
 
-	const xor = (arr) => {
-		return arr.reduce((a, c) => c ^ a, 0);
-	};
+  const xor = (arr) => {
+    return arr.reduce((a, c) => c ^ a, 0);
+  };
 
-	let answer = 0;
-	subsets.map((arr) => {
-		answer += xor(arr);
-	});
-
-	return answer;
+  return subsets.reduce((a, c) => xor(c) + a, 0);
 };
