5a7ed69e4f3a84d942132d95a186419b7552328d
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2022-10-07 10:14:22 +0900

    Time: 226 ms (19.51%), Space: 57.2 MB (5.49%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.js
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.js
@@ -3,15 +3,14 @@
  * @return {number}
  */
 var numberOfBeams = function(bank) {
+  bank = bank.filter((line) => line.includes('1'));
+	if (bank.length === 1) return 0;
+
 	const countLaser = (line) => {
 		return [...line].filter((laser) => laser === '1').length;
 	};
 
-	bank = bank.filter((line) => !!countLaser(line));
-
 	let answer = 0;
-	if (bank.length === 1) return answer;
-
 	for (let i = 0; i < bank.length - 1; i++) {
 		answer += countLaser(bank[i]) * countLaser(bank[i + 1]);
 	}
