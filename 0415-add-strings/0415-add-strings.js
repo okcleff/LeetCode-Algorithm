84790d09075857f52e36a952011769cb5214cfc3
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2022-12-18 09:59:09 +0900

    Time: 91 ms (73.62%), Space: 44.6 MB (52.38%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function(num1, num2) {
+  let num1Len = num1.length;
+  let num2Len = num2.length;
+  let maxLen = num1Len > num2Len ? num1Len : num2Len;
+  maxLen++;
+  
+  let carry = 0;
+  let total = '';
+  
+  for (let i = 0; i < maxLen; i++) {
+    if (i === maxLen - 1 && carry === 0) {
+      continue;
+    }
+    let pos = num1Len - i - 1;
+    let a = pos < 0 ? 0 : num1.charCodeAt(pos) - 48;
+    pos = num2Len - i - 1;
+    let b = pos < 0 ? 0 : num2.charCodeAt(pos) - 48;
+    let t = a + b + carry;
+    carry = Math.floor(t / 10);
+    total = (t % 10) + total;
+  }
+  
+  return total;
+};
