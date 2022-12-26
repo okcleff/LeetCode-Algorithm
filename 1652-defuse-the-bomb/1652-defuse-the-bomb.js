c5079689c326446c0f0b5bcb4c1f3267cc232641
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2022-12-26 16:06:12 +0900

    Time: 73 ms (81.25%), Space: 42.9 MB (37.50%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} code
+ * @param {number} k
+ * @return {number[]}
+ */
+var decrypt = function(code, k) {
+	const answer = new Array(code.length).fill(0);
+
+	if (k > 0) {
+		for (let i = 0; i < code.length; i++) {
+			let count = 0;
+			let j = i + 1;
+			while (count < k) {
+				if (j === code.length) j = 0;
+				answer[i] += code[j];
+				count = count + 1;
+				j++;
+			}
+		}
+	}
+  
+	if (k < 0) {
+		for (let i = 0; i < code.length; i++) {
+			let count = 0;
+			let j = i - 1;
+			while (count > k) {
+				if (j === -1) j = code.length - 1;
+				answer[i] += code[j];
+				count = count - 1;
+				j--;
+			}
+		}
+	}
+  
+	return answer;
+};
