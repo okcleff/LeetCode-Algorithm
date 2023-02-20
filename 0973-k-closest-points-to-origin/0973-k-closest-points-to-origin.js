ea52925bb734bb396ede1f980ef794dbac18bbd1
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2023-02-20 09:46:37 +0900

    Time: 206 ms (79.33%), Space: 52.6 MB (87.15%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[][]} points
+ * @param {number} k
+ * @return {number[][]}
+ */
+var kClosest = function(points, k) {
+	return points
+		.sort((a, b) => a[0] ** 2 + a[1] ** 2 - (b[0] ** 2 + b[1] ** 2))
+		.slice(0, k);
+};
