a937d2ce7fbe53334ebcea6e5f000a6886fdfb72
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2023-02-27 09:53:43 +0900

    Time: 134 ms (33.33%), Space: 49.6 MB (33.33%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.js
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.js
@@ -6,12 +6,12 @@ var leftRigthDifference = function(nums) {
 	const answer = [];
   
 	for (let i = 0; i < nums.length; i++) {
-		let leftSum =
+		const leftSum =
 			nums.slice(0, i).length === 0
 				? 0
 				: nums.slice(0, i).reduce((a, b) => a + b);
     
-		let rightSum =
+		const rightSum =
 			nums.slice(i + 1).length === 0
 				? 0
 				: nums.slice(i + 1).reduce((a, b) => a + b);
