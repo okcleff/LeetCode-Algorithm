a9b21208a7eb706692b47dfc69dda77dedc86c27
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2023-03-02 10:02:22 +0900

    Time: 78 ms (36.16%), Space: 45 MB (11.69%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.js
+++ 0941-valid-mountain-array/0941-valid-mountain-array.js
@@ -5,16 +5,16 @@
 var validMountainArray = function(arr) {
 	if (arr.length <= 2) return false;
 
-	const peakIdx = arr.indexOf(Math.max(...arr));
+	const peak = arr.indexOf(Math.max(...arr));
 
-	if (peakIdx === 0 || peakIdx === arr.length - 1) return false;
+	if (peak === 0 || peak === arr.length - 1) return false;
 
-	const uphill = arr.slice(0, peakIdx);
+	const uphill = arr.slice(0, peak);
 	for (let i = 0; i < uphill.length - 1; i++) {
 		if (uphill[i] >= uphill[i + 1]) return false;
 	}
 
-	const downhill = arr.slice(peakIdx);
+	const downhill = arr.slice(peak);
 	for (let i = 0; i < downhill.length - 1; i++) {
 		if (downhill[i] <= downhill[i + 1]) return false;
 	}
