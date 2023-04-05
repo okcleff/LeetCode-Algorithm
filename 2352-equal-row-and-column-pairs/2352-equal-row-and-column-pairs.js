217c4f23c2fac3fba7c5cd8a468eba81e705cbaf
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2023-04-05 09:22:16 +0900

    Time: 214 ms (18.56%), Space: 53.4 MB (55.67%) - LeetHub

---
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var equalPairs = function(grid) {
+	const leng = grid.length;
+	const rows = grid.map((row) => row.join('.'));
+	const cols = [];
+
+	for (let i = 0; i < leng; i++) {
+		let col = '';
+		for (let j = 0; j < leng; j++) {
+			col += col ? '.' + grid[j][i] : grid[j][i];
+		}
+		cols.push(col);
+	}
+
+	let answer = 0;
+
+	for (let i = 0; i < leng; i++) {
+		for (let j = 0; j < leng; j++) {
+			if (rows[i] === cols[j]) answer++;
+		}
+	}
+
+	return answer;
+};
