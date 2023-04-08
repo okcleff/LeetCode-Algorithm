db03648646a079ef8203a5b2b107ae69918fa561
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2023-04-08 09:10:59 +0900

    Time: 116 ms (47.72%), Space: 49.7 MB (55.81%) - LeetHub

---
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var partitionString = function(s) {
+  let map = new Map();
+  let answer = 1;
+
+  for (let i = 0; i < s.length; i++) {
+    if (map.has(s[i])) {
+      answer++;
+      map = new Map();
+    }
+    map.set(s[i], 1);
+  }
+
+  return answer;
+};
