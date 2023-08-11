002ce86ab944bd31f46df3d980b10ec2f0754093
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2023-08-11 10:07:55 +0900

    Time: 158 ms (99.28%), Space: 68 MB (32.13%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.js
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.js
@@ -8,7 +8,7 @@ var sortVowels = function(s) {
   const set = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
   const arr = s.split('');
 
-  for (let i = 0; i < s.length; ++i) {
+  for (let i = 0; i < s.length; i++) {
     if (set.has(s[i])) {
       vowels.push(s[i]);
       indexes.push(i);
@@ -17,7 +17,7 @@ var sortVowels = function(s) {
 
   vowels.sort((a, b) => a.charCodeAt() - b.charCodeAt());
 
-  for (let i = 0; i < indexes.length; ++i) {
+  for (let i = 0; i < indexes.length; i++) {
     arr[indexes[i]] = vowels[i];
   }
 
