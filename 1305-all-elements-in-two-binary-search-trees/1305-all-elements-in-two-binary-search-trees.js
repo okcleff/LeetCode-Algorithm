b17479063aecf2d055c1bf182444cd65290942ab
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2023-10-31 10:11:13 +0900

    Time: 127 ms (95.65%), Space: 59.4 MB (75.16%) - LeetHub

---
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {number[]}
+ */
+var getAllElements = function(root1, root2) {
+  const answer = [];
+  
+  const dfs = (root) => {
+    if (!root) return null;
+    
+    if (root) {
+      answer.push(root.val);
+    }
+    
+    if (root.left !== null) dfs(root.left);
+    if (root.right !== null) dfs(root.right);
+  }
+  
+  dfs(root1);
+  dfs(root2);
+  
+  return answer.sort((a, b) => a - b);
+};
