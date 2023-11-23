526d7631e0c02cd22eb5906f76ec07b22aca059e
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2023-11-23 09:32:43 +0900

    Time: 49 ms (70.39%), Space: 41.2 MB (98.63%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+  const answer = [];
+  
+  const traverse = (node) => {
+    if (!node) return;
+    answer.push(node.val);
+    
+    traverse(node.left);
+    traverse(node.right);
+  }
+  
+  traverse(root);
+  
+  return answer;
+};
