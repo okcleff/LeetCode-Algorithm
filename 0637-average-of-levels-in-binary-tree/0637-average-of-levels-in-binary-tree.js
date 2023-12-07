6e9c67ee393f92c4e988daf63bc0ddebcae32c41
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2023-12-07 09:27:23 +0900

    Time: 62 ms (82.25%), Space: 46.7 MB (81.06%) - LeetHub

---
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var averageOfLevels = function(root) {
+  const answer = [];
+  const queue = [root];
+  
+  while (queue.length) {
+    let sum = 0;
+    const leng = queue.length;
+    for (let i = 0; i < leng; i++) {
+      const node = queue.shift();
+      sum += node.val;
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    answer.push(sum / leng);
+  }
+  
+  return answer;
+};
