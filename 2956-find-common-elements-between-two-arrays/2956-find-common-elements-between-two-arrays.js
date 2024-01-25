53b72ec4b4e51aa1ca270945a9a4c01425133dcb
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2024-01-25 09:51:32 +0900

    Time: 84 ms (30.40%), Space: 53.1 MB (5.05%) - LeetHub

--- 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.js
+++ 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.js
@@ -7,13 +7,13 @@ var findIntersectionValues = function(nums1, nums2) {
   let nums1Count = 0;
   let nums2Count = 0;
   
-  for (let i = 0; i < nums1.length; i++) {
-    if(nums2.includes(nums1[i])) nums1Count++;
+  const countCommonElements = (arr1, arr2) => {
+    let count = 0;
+    for (let i = 0; i < arr1.length; i++) {
+      if(arr2.includes(arr1[i])) count++;
+    }
+    return count;
   }
   
-  for (let i = 0; i < nums2.length; i++) {
-    if(nums1.includes(nums2[i])) nums2Count++;
-  }
-  
-  return [nums1Count, nums2Count];
+  return [countCommonElements(nums1, nums2), countCommonElements(nums2, nums1)];
 };
