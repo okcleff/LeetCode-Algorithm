19a941614f96a4254bbf4ed085869b1e7d29de88
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2024-02-27 17:08:07 +0900

    Time: 95 ms (31.93%), Space: 57.4 MB (25.90%) - LeetHub

---
+++ 0654-maximum-binary-tree/0654-maximum-binary-tree.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var constructMaximumBinaryTree = function(nums) {
+  if (!nums.length) {
+    return null;
+  }
+
+  const max = Math.max(...nums);
+  let root = new TreeNode(max);
+  let rootIndex = nums.findIndex(num => num === max);
+  root.left = constructMaximumBinaryTree(nums.slice(0, rootIndex));
+  root.right = constructMaximumBinaryTree(nums.slice(rootIndex + 1, nums.length));
+  
+  return root;
+};
