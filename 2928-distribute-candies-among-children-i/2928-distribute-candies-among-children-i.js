e32424ffa1a937535b7a9c2cb66d822691973159
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2024-03-18 09:57:48 +0900

    Time: 87 ms (22.04%), Space: 49.8 MB (59.42%) - LeetHub

---
+++ 2928-distribute-candies-among-children-i/2928-distribute-candies-among-children-i.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @param {number} limit
+ * @return {number}
+ */
+var distributeCandies = function(n, limit) {
+  let answer = 0;
+
+  for (let i = 0; i <= limit; i++) {
+    for (let j = 0; j <= limit; j++) {
+      for (let k = 0; k <= limit; k++) {
+        if (i + j + k === n) {
+          answer++;
+        }
+      }
+    }
+  }
+
+  return answer;
+};
