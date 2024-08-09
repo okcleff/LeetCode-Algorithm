356de16339fdd88f0a1f2e5fceca312c832e7b6f
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2024-08-09 15:47:26 +0900

    Time: 77 ms (66.00%), Space: 57.6 MB (98.29%) - LeetHub

---
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {boolean}
+ */
+var findTarget = function(root, k) {
+  if (!root) return false;
+  const set = new Set();
+  const stack = [root];
+  
+  while (stack.length) {
+    const node = stack.pop();
+    if (set.has(k - node.val)) return true;
+    
+    set.add(node.val);
+    
+    if (node.right) stack.push(node.right);
+    if (node.left) stack.push(node.left);
+  }
+  
+  return false;
+};
