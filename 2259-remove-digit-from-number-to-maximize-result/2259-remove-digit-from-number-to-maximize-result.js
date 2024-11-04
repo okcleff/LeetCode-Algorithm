c0a2406e80d8f99e5c960123f9fb235372b1e50e
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2024-11-04 13:40:10 +0900

    Time: 0 ms (100.00%), Space: 49.2 MB (79.03%) - LeetHub

--- 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.js
+++ 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.js
@@ -4,20 +4,17 @@
  * @return {string}
  */
 var removeDigit = function(number, digit) {
-    let maxResult = '0';
+  let answer = '0';
     
-    // 문자열의 각 위치에서 digit을 찾아 제거해보고 최대값 찾기
-    for (let i = 0; i < number.length; i++) {
-        if (number[i] === digit) {
-            // 현재 위치의 숫자를 제거한 새로운 문자열 생성
-            let newNum = number.slice(0, i) + number.slice(i + 1);
-            
-            // 현재까지의 최대값과 비교
-            if (newNum > maxResult) {
-                maxResult = newNum;
-            }
-        }
+    
+  for (let i = 0; i < number.length; i++) {
+    if (number[i] === digit) {
+      let newNum = number.slice(0, i) + number.slice(i + 1);
+      if (newNum > answer) {
+        answer = newNum;
+      }
     }
+  }
     
-    return maxResult;
+  return answer;
 };
