6f324c885e0f6fb9187ed0532d1803fbc5d21d6d
Woncheol, Ok <85351198+okcleff@users.noreply.github.com>
2024-11-27 13:26:01 +0900

    Time: 230 ms (6.67%), Space: 108.5 MB (96.77%) - LeetHub

--- 3295-report-spam-message/3295-report-spam-message.ts
+++ 3295-report-spam-message/3295-report-spam-message.ts
@@ -1,30 +1,32 @@
 function reportSpam(message, bannedWords) {
-    bannedWords.sort(); // O(n log n) 정렬
-    let bannedCount = 0;
+  bannedWords.sort();
+  
+  let bannedCount = 0;
 
-    for (const word of message) {
-        if (binarySearch(bannedWords, word)) {
-            bannedCount++;
-            if (bannedCount >= 2) {
-                return true;
-            }
-        }
+  for (const word of message) {
+    if (binarySearch(bannedWords, word)) bannedCount++;
+    if (bannedCount === 2) {
+      return true;
     }
+  }
 
-    return false;
+  return false;
 }
 
 function binarySearch(array, target) {
-    let left = 0, right = array.length - 1;
-    while (left <= right) {
-        const mid = Math.floor((left + right) / 2);
-        if (array[mid] === target) {
-            return true;
-        } else if (array[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
+  let left = 0, right = array.length - 1;
+  
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (array[mid] === target) {
+      return true;
+    } else if (array[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
     }
-    return false;
+  }
+
+  return false;
 }
